69c8fb26a9d99110971995b57eede62df548ee88
kkm <kubasurdej@gmail.com>
2020-04-15 18:43:17 +0200

    Extend keywords with lookupIdentifier function

---
+++ src/lexer/lexer.ts
@@ -0,0 +1,6 @@
+/*export class Lexer {
+  private input: string;
+  private position: number;
+  private readPosition: number;
+}
+*/
--- src/tokens.ts
+++
@@ -1,9 +0,0 @@
-export const tokens = {
-  INT: "INT",
-  VAR: "VAR",
-  IDENT: "IDENT",
-  LPAREN: "(",
-  RPAREN: ")",
-  ASSIGN: "=",
-  WHILE: "WHILE",
-};
