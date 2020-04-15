041e2649bdbbda494d7107a51eb3b4b0172709b5
kkm <kubasurdej@gmail.com>
2020-04-15 18:22:48 +0200

    Add lookupIdent function

--- src/keywords.ts
+++ src/keywords.ts
@@ -3,3 +3,11 @@ import { tokens } from "./tokens";
 export const keywords: Map<string, string> = new Map();
 keywords.set("while", tokens.WHILE);
 keywords.set("var", tokens.VAR);
+
+export const lookupIdent = (ident: string): string => {
+  const keyword = keywords.get(ident);
+  if (keyword) {
+    return keyword;
+  }
+  return tokens.IDENT;
+};
