06c701a2005ab0f673ecbcc363e64076f1d0682e
kkm <kubasurdej@gmail.com>
2020-04-16 16:19:12 +0200

    Create interfaces for AST

--- src/parser/ast.ts
+++ src/parser/ast.ts
@@ -1,6 +1,26 @@
-interface Node {
+import { Token } from "../lexer/tokens";
+
+export type NodeType = "expression" | "statement";
+
+export interface Node {
   type: NodeType;
   tokenLiteral: string;
 }
 
-type NodeType = "expression" | "statement";
+export interface Statement extends Node {
+  type: "statement";
+}
+
+export interface Expression extends Node {
+  type: "expression";
+}
+
+export interface Identifier {
+  token: Token;
+  value: string;
+}
+
+export interface VarStatement extends Statement {
+  name: Identifier;
+  value: Expression;
+}
